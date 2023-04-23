d459170ea3d301b7705d238a3cadf90578cb5663
lunarkh <ed.doubleav@gmail.com>
2023-04-23 22:22:10 +0700

    add crc-5/G-704

---
+++ crc_5_g_704.m
@@ -0,0 +1,32 @@
+function out = crc_5_g_704(message, check)
+%--settings-------
+% width   = 5
+% poly    = 0x15
+% init    = 0x00
+% refin   = true
+% refout  = true
+% xorout  = 0x00
+% check   = 0x07 for ASCII:"123456789"
+% residue = 0x00
+% name    = "CRC-5/G-704"
+% Class: academic
+% Alias: CRC-5/ITU
+% ITU-T Recommendation G.704 (October 1998)
+% - Definition: Residue; full mathematical description (Section 2.2.3.2, pp.8–9)
+% - Shift register diagram (Annex A.2, p.36)
+
+crc.width   = 5;
+crc.poly    = [1 0 1 0 1];% x^5 + x^4 + x^2 + x^0 (0x15)
+crc.init    = false(1,length(crc.poly));% [0 0 0 0 0];% (0x00)
+crc.residue = false(1,length(crc.poly));% [0 0 0 0 0];% (0x00)
+crc.refin   = true;
+crc.refout  = true;
+crc.xorout  = false(1,length(crc.poly));% [0 0 0 0 0];% (0x00)
+crc.mode    = check;
+crc.message = message;
+
+crc.calc_bin = crc_uni(crc.width, crc.poly, crc.init, crc.residue, crc.refin, crc.refout, crc.xorout, crc.mode, crc.message);
+
+out =  binaryVectorToHex(crc.calc_bin);
+
+end
--- main.m
+++ main.m
@@ -17,8 +17,12 @@ crc_hex = crc_4_g_704(message, false); disp(" - calc  = 0x" + crc_hex); disp(" "
 disp(" CRC-4/INTERLAKEN");% ASCII:"123456789" crc_check:"0xb"
 crc_hex = crc_4_interlaken(message, false); disp(" - calc  = 0x" + crc_hex); disp(" ");
 
-disp(" CRC-5/EPC-C1G2");% ASCII:"123456789" crc_check:"0xb"
+disp(" CRC-5/EPC-C1G2");% ASCII:"123456789" crc_check:"0x00"
 crc_hex = crc_5_epc_c1g2(message, false); disp(" - calc  = 0x" + crc_hex); disp(" ");
+
+disp(" CRC-5/G-704");% ASCII:"123456789" crc_check:"0x07"
+crc_hex = crc_5_g_704(message, false); disp(" - calc  = 0x" + crc_hex);
+crc_hex = crc_5_g_704([message crc_hex], true); disp(" - check = 0x" + crc_hex); disp(" ");
 
 disp(" CRC-6/G-704. Alias: CRC-6/ITU");% ASCII:"123456789" crc_check:"0x06"
 crc_hex = crc_6_g_704(message, false);          disp(" - calc  = 0x" + crc_hex);
